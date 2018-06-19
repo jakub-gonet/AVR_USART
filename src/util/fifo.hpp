3ff046cb32574ac8fe6d5876545bfaeb8b9f4ed8
Jakub Gonet <jakub.gonet0@gmail.com>
2018-06-19 11:27:42 +0200

    Add take_from_queue function

--- src/util/fifo.hpp
+++ src/util/fifo.hpp
@@ -20,6 +20,26 @@ class FifoQueue {
     }
     return false;
   }
+
+  /**
+   * @brief Returns next element from queue
+   *
+   * @param queue_empty when true, indicates that queue is empty, so it can't
+   * provide next element
+   * @return const uint8_t
+   */
+  const uint8_t take_from_queue(bool& error) {
+    if (next_queue_element_index <= last_queue_element_index) {
+      queue_empty = false;
+      return sending_queue[next_queue_element_index++];
+    }
+    next_queue_element_index = 0;
+    last_queue_element_index = 0;
+
+    queue_empty = true;
+    return 0;
+  }
+
  private:
   uint8_t fifo_size = Size;
   uint8_t sending_queue[Size];
