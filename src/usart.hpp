0387cc67184989e0976a3cf5a90a8c066b1b98c8
Jakub Gonet <jakub.gonet0@gmail.com>
2018-06-19 14:44:57 +0200

    Add functions to start data transmissions via interrupts

--- src/usart.hpp
+++ src/usart.hpp
@@ -23,6 +23,28 @@ class Usart {
    *
    */
   FifoQueue<32> received;
+
+  /**
+   * @brief Start the proccess of sending data from `to_send` queue. It should
+   * end after reaching delimiter or if queue is empty
+   *
+   */
+  inline void start_sending_data() const {
+    wait_until_transmit_buffer_is_ready();
+    enable_transmit_buffer_empty_interrupts();
+  }
+
+  inline void wait_until_transmit_buffer_is_ready() const {
+    while (!(UCSRA & (1 << UDRE)))
+      ;
+  }
+  inline void enable_transmit_buffer_empty_interrupts() const {
+    UCSRB |= (1 << UDRIE);
+  }
+
+  inline void disable_transmit_buffer_empty_interrupts() const {
+    UCSRB &= ~(1 << UDRIE);
+  }
 };
 
 #endif  // USART_HPP
