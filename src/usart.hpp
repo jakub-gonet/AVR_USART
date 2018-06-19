8c9744dd0e4c7af3bd4e10a8c7c9b27b1fe3dde0
Jakub Gonet <jakub.gonet0@gmail.com>
2018-06-19 11:30:00 +0200

    Add USART functions declaration

--- src/usart.hpp
+++ src/usart.hpp
@@ -2,6 +2,16 @@
 #define USART_HPP
 
 class Usart {
+ public:
+  Usart(const uint16_t baud_rate);
+  const bool send_string(char const* const buffer,
+                         const uint8_t buffer_size) const;
+  const char* const receive_string(char* const buffer,
+                                   const uint8_t buffer_size) const;
+
+ private:
+  const bool send_byte(const uint8_t byte) const;
+  const uint8_t receive_byte() const;
 };
 
 #endif  // USART_HPP
