0cd6f4de410128eaa0ebb23fbd624625a7ea354c
Jakub Gonet <jakub.gonet0@gmail.com>
2018-06-19 14:46:39 +0200

    Add function to send string via USART

--- src/usart.cpp
+++ src/usart.cpp
@@ -1,5 +1,6 @@
 #include "usart.hpp"
 #include <avr/io.h>
+#include "util/fifo.hpp"
 
 Usart::Usart(uint16_t baud_rate) {
   UBRRH = (baud_rate >> 8);
@@ -10,4 +11,13 @@ Usart::Usart(uint16_t baud_rate) {
 
   // set data format to 8 bits, 1 stop bit, no parity check
   UCSRC = (1 << URSEL) | (1 << UCSZ0) | (1 << UCSZ1);
+}
+
+const bool Usart::send_string(char const* const buffer,
+                              const uint8_t buffer_size) {
+  for (uint8_t i = 0; i < buffer_size; ++i) {
+    to_send.push_to_queue(static_cast<uint8_t>(buffer[i]));
+  }
+
+  start_sending_data();
 }
--- src/usart.hpp
+++ src/usart.hpp
@@ -6,8 +6,7 @@
 class Usart {
  public:
   Usart(const uint16_t baud_rate);
-  const bool send_string(char const* const buffer,
-                         const uint8_t buffer_size) const;
+  const bool send_string(char const* const buffer, const uint8_t buffer_size);
   const char* const receive_string(char* const buffer,
                                    const uint8_t buffer_size) const;
   constexpr const char get_string_delimiter() const { return '\0'; }
