7b544cd23214d1bd045731ae1fae74f2258c52c9
Jakub Gonet <jakub.gonet0@gmail.com>
2018-06-19 11:26:56 +0200

    Add container to FIFO

--- src/util/fifo.hpp
+++ src/util/fifo.hpp
@@ -1,6 +1,14 @@
 #ifndef FIFO_HPP
 #define FIFO_HPP
 
+#include <stdint.h>
+
+template <uint8_t Size = 32>
 class FifoQueue {
+ private:
+  uint8_t fifo_size = Size;
+  uint8_t sending_queue[Size];
+  uint8_t next_queue_element_index = 0;
+  uint8_t last_queue_element_index = 0;
 };
 #endif  // FIFO_HPP
