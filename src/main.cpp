9ba3625a345f846b03382025b6e619c3334c6ad0
Jakub Gonet <jakub.gonet0@gmail.com>
2018-06-20 08:29:22 +0200

    Initialize USART in main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,21 @@
+extern "C" {
+#include <avr/interrupt.h>
 #include <avr/io.h>
+}
+#include "usart.hpp"
+
+Usart usart(4800);
 
 int main() {
+  sei();
   while (true) {
   }
+}
+
+ISR(USART_UDRE_vect) {
+  usart.handle_send_interrupt();
+}
+
+ISR(USART_RXC_vect) {
+  usart.handle_receive_interrupt();
 }
